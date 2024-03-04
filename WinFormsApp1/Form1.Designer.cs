3bdcc9dbd05b789aad671a2679737e9ea17f6414
Jjav2 <Jjav2@HPCOMPUTER18>
2024-03-03 19:01:55 -0600

    Agregar archivos de proyecto.

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,316 @@
+﻿namespace WinFormsApp1
+{
+  partial class Form1
+  {
+    /// <summary>
+    ///  Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    ///  Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btn1 = new Button();
+            btn2 = new Button();
+            btn3 = new Button();
+            btn6 = new Button();
+            btn5 = new Button();
+            btn4 = new Button();
+            btn9 = new Button();
+            btn8 = new Button();
+            btn7 = new Button();
+            txtPantalla = new TextBox();
+            btnSuma = new Button();
+            btnIgual = new Button();
+            btn0 = new Button();
+            btnPunto = new Button();
+            btnResta = new Button();
+            btnMultp = new Button();
+            btnDivis = new Button();
+            btnLimpiar = new Button();
+            SuspendLayout();
+            // 
+            // btn1
+            // 
+            btn1.Font = new Font("Stencil", 15F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            btn1.Location = new Point(363, 323);
+            btn1.Margin = new Padding(3, 4, 3, 4);
+            btn1.Name = "btn1";
+            btn1.Size = new Size(57, 67);
+            btn1.TabIndex = 0;
+            btn1.Text = "1";
+            btn1.UseVisualStyleBackColor = true;
+            btn1.Click += btn_Click;
+            // 
+            // btn2
+            // 
+            btn2.Font = new Font("Stencil", 15F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            btn2.Location = new Point(427, 323);
+            btn2.Margin = new Padding(3, 4, 3, 4);
+            btn2.Name = "btn2";
+            btn2.Size = new Size(57, 67);
+            btn2.TabIndex = 1;
+            btn2.Text = "2";
+            btn2.UseVisualStyleBackColor = true;
+            btn2.Click += btn_Click;
+            // 
+            // btn3
+            // 
+            btn3.Font = new Font("Stencil", 15F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            btn3.Location = new Point(491, 323);
+            btn3.Margin = new Padding(3, 4, 3, 4);
+            btn3.Name = "btn3";
+            btn3.Size = new Size(57, 67);
+            btn3.TabIndex = 2;
+            btn3.Text = "3";
+            btn3.UseVisualStyleBackColor = true;
+            btn3.Click += btn_Click;
+            // 
+            // btn6
+            // 
+            btn6.Font = new Font("Stencil", 15F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            btn6.Location = new Point(491, 248);
+            btn6.Margin = new Padding(3, 4, 3, 4);
+            btn6.Name = "btn6";
+            btn6.Size = new Size(57, 67);
+            btn6.TabIndex = 5;
+            btn6.Text = "6";
+            btn6.UseVisualStyleBackColor = true;
+            btn6.Click += btn_Click;
+            // 
+            // btn5
+            // 
+            btn5.Font = new Font("Stencil", 15F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            btn5.Location = new Point(427, 248);
+            btn5.Margin = new Padding(3, 4, 3, 4);
+            btn5.Name = "btn5";
+            btn5.Size = new Size(57, 67);
+            btn5.TabIndex = 4;
+            btn5.Text = "5";
+            btn5.UseVisualStyleBackColor = true;
+            btn5.Click += btn_Click;
+            // 
+            // btn4
+            // 
+            btn4.Font = new Font("Stencil", 15F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            btn4.Location = new Point(363, 248);
+            btn4.Margin = new Padding(3, 4, 3, 4);
+            btn4.Name = "btn4";
+            btn4.Size = new Size(57, 67);
+            btn4.TabIndex = 3;
+            btn4.Text = "4";
+            btn4.UseVisualStyleBackColor = true;
+            btn4.Click += btn_Click;
+            // 
+            // btn9
+            // 
+            btn9.Font = new Font("Stencil", 15F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            btn9.Location = new Point(491, 173);
+            btn9.Margin = new Padding(3, 4, 3, 4);
+            btn9.Name = "btn9";
+            btn9.Size = new Size(57, 67);
+            btn9.TabIndex = 8;
+            btn9.Text = "9";
+            btn9.UseVisualStyleBackColor = true;
+            btn9.Click += btn_Click;
+            // 
+            // btn8
+            // 
+            btn8.Font = new Font("Stencil", 15F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            btn8.Location = new Point(427, 173);
+            btn8.Margin = new Padding(3, 4, 3, 4);
+            btn8.Name = "btn8";
+            btn8.Size = new Size(57, 67);
+            btn8.TabIndex = 7;
+            btn8.Text = "8";
+            btn8.UseVisualStyleBackColor = true;
+            btn8.Click += btn_Click;
+            // 
+            // btn7
+            // 
+            btn7.Font = new Font("Stencil", 15F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            btn7.Location = new Point(363, 173);
+            btn7.Margin = new Padding(3, 4, 3, 4);
+            btn7.Name = "btn7";
+            btn7.Size = new Size(57, 67);
+            btn7.TabIndex = 6;
+            btn7.Text = "7";
+            btn7.UseVisualStyleBackColor = true;
+            btn7.Click += btn_Click;
+            // 
+            // txtPantalla
+            // 
+            txtPantalla.BackColor = SystemColors.InactiveCaption;
+            txtPantalla.Font = new Font("Segoe UI", 20F);
+            txtPantalla.Location = new Point(363, 108);
+            txtPantalla.Margin = new Padding(3, 4, 3, 4);
+            txtPantalla.Name = "txtPantalla";
+            txtPantalla.Size = new Size(335, 52);
+            txtPantalla.TabIndex = 9;
+            txtPantalla.TextAlign = HorizontalAlignment.Right;
+            // 
+            // btnSuma
+            // 
+            btnSuma.Font = new Font("Stencil", 15F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            btnSuma.Location = new Point(555, 173);
+            btnSuma.Margin = new Padding(3, 4, 3, 4);
+            btnSuma.Name = "btnSuma";
+            btnSuma.Size = new Size(57, 67);
+            btnSuma.TabIndex = 10;
+            btnSuma.Text = "+";
+            btnSuma.UseVisualStyleBackColor = true;
+            btnSuma.Click += btnOperador_Click;
+            // 
+            // btnIgual
+            // 
+            btnIgual.Font = new Font("Stencil", 15F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            btnIgual.Location = new Point(627, 276);
+            btnIgual.Margin = new Padding(3, 4, 3, 4);
+            btnIgual.Name = "btnIgual";
+            btnIgual.Size = new Size(71, 188);
+            btnIgual.TabIndex = 11;
+            btnIgual.Text = "=";
+            btnIgual.UseVisualStyleBackColor = true;
+            btnIgual.Click += btnIgual_Click;
+            // 
+            // btn0
+            // 
+            btn0.Font = new Font("Stencil", 15F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            btn0.Location = new Point(363, 397);
+            btn0.Margin = new Padding(3, 4, 3, 4);
+            btn0.Name = "btn0";
+            btn0.Size = new Size(121, 67);
+            btn0.TabIndex = 12;
+            btn0.Text = "0";
+            btn0.UseVisualStyleBackColor = true;
+            btn0.Click += btn_Click;
+            // 
+            // btnPunto
+            // 
+            btnPunto.Font = new Font("Stencil", 15F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            btnPunto.Location = new Point(491, 397);
+            btnPunto.Margin = new Padding(3, 4, 3, 4);
+            btnPunto.Name = "btnPunto";
+            btnPunto.Size = new Size(57, 67);
+            btnPunto.TabIndex = 13;
+            btnPunto.Text = ".";
+            btnPunto.UseVisualStyleBackColor = true;
+            btnPunto.Click += btn_Click;
+            // 
+            // btnResta
+            // 
+            btnResta.Font = new Font("Stencil", 15F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            btnResta.Location = new Point(555, 248);
+            btnResta.Margin = new Padding(3, 4, 3, 4);
+            btnResta.Name = "btnResta";
+            btnResta.Size = new Size(57, 67);
+            btnResta.TabIndex = 14;
+            btnResta.Text = "-";
+            btnResta.UseVisualStyleBackColor = true;
+            btnResta.Click += btnOperador_Click;
+            // 
+            // btnMultp
+            // 
+            btnMultp.Font = new Font("Stencil", 15F, FontStyle.Italic);
+            btnMultp.Location = new Point(555, 323);
+            btnMultp.Name = "btnMultp";
+            btnMultp.Size = new Size(57, 67);
+            btnMultp.TabIndex = 15;
+            btnMultp.Text = "*";
+            btnMultp.UseVisualStyleBackColor = true;
+            btnMultp.Click += btnOperador_Click;
+            // 
+            // btnDivis
+            // 
+            btnDivis.Font = new Font("Stencil", 15F, FontStyle.Italic);
+            btnDivis.Location = new Point(555, 397);
+            btnDivis.Name = "btnDivis";
+            btnDivis.Size = new Size(57, 67);
+            btnDivis.TabIndex = 16;
+            btnDivis.Text = "/";
+            btnDivis.UseVisualStyleBackColor = true;
+            btnDivis.Click += btnOperador_Click;
+            // 
+            // btnLimpiar
+            // 
+            btnLimpiar.Font = new Font("Stencil", 15F, FontStyle.Italic);
+            btnLimpiar.ForeColor = Color.Red;
+            btnLimpiar.Location = new Point(627, 173);
+            btnLimpiar.Name = "btnLimpiar";
+            btnLimpiar.Size = new Size(71, 82);
+            btnLimpiar.TabIndex = 17;
+            btnLimpiar.Text = "C";
+            btnLimpiar.UseVisualStyleBackColor = true;
+            btnLimpiar.Click += btnLimpiar_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(255, 192, 128);
+            ClientSize = new Size(944, 562);
+            Controls.Add(btnLimpiar);
+            Controls.Add(btnDivis);
+            Controls.Add(btnMultp);
+            Controls.Add(btnResta);
+            Controls.Add(btnPunto);
+            Controls.Add(btn0);
+            Controls.Add(btnIgual);
+            Controls.Add(btnSuma);
+            Controls.Add(txtPantalla);
+            Controls.Add(btn9);
+            Controls.Add(btn8);
+            Controls.Add(btn7);
+            Controls.Add(btn6);
+            Controls.Add(btn5);
+            Controls.Add(btn4);
+            Controls.Add(btn3);
+            Controls.Add(btn2);
+            Controls.Add(btn1);
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btn1;
+    private Button btn2;
+    private Button btn3;
+    private Button btn6;
+    private Button btn5;
+    private Button btn4;
+    private Button btn9;
+    private Button btn8;
+    private Button btn7;
+    private TextBox txtPantalla;
+    private Button btnSuma;
+    private Button btnIgual;
+    private Button btn0;
+    private Button btnPunto;
+    private Button btnResta;
+    private Button btnMultp;
+    private Button btnDivis;
+        private Button btnLimpiar;
+    }
+}
